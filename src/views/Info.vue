e6df28f33c09f29a5df4cb4ad41ee42b9ed181fa
latsic <latsic@quantentunnel.de>
2019-02-01 11:17:32 +0000

    correct spelling errors

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -6,7 +6,7 @@
 
       <p class="text-xs-left">
         A client-side only <strong>webapplication</strong> to <strong>join, stitch or align</strong> overlapping images.
-        It can be used to created <strong>panoramas</strong> or to <strong>align images</strong> (for example forms which have not been scanned properly).
+        It can be used to create <strong>panoramas</strong> or to <strong>align images</strong>, for example forms which have not been scanned properly.
       </p>
       
       <p class="text-xs-left">
@@ -22,14 +22,14 @@
         <a href="https://github.com/opencv/opencv/tree/master/modules/features2d">Feature2D</a> and
         <a href="https://github.com/opencv/opencv/tree/master/modules/stitching">Stiching</a> mainly)
         and have been adjusted where needed. A customized version of OpenCV
-        has than been compiled to <strong>wasm</strong>. To ensure a non blocking UI, all OpenCV related function run via a <strong>webworker</strong>.
+        has then been compiled to <strong>wasm</strong>. To ensure a non blocking UI, all OpenCV related functions run via a <strong>webworker</strong>.
       </p>
       
       <p class="text-xs-left">
         <strong>Pros and cons:</strong> As image stiching can be quite <strong>memory and cpu intensive</strong>, there are some limitations
-        on the amount or sizes of images which can be stitched together, especially on mobile devices.
+        on the amount or size of images which can be stitched together, especially on mobile devices.
         On the plus side, basic stitching functionality is available on almost any
-        devices which can run a browser. There is no need to download a professional stitching software.
+        device which can run a browser. There is no need to download a professional stitching software.
         The application is <strong>fully functional offline</strong> as well.
       </p>
 
